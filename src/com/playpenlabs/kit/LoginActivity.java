df6445023b403d0ad42496573036f3bb33b69342
Diyah <me@hadiyah.me>
2013-11-14 19:26:54 -0800

    add parse

--- src/com/playpenlabs/kit/LoginActivity.java
+++ src/com/playpenlabs/kit/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.playpenlabs.kit;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "22DcKkWatn1Dz6bWQZHKZixGHJ0YTjNzLtWF1oDC", "w5tzJYX5hhSqZ6sF83TVdVNsA1G3Yma5iK3v4wv1");
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
 		setContentView(R.layout.activity_login);
 	}
 
